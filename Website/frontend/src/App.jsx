f184388ed6aaec7db54439509b65042d718da244
Pavlo <pas.haors79@gmail.com>
2025-02-23 11:45:21 +0100

    Initialized Routing and added main components and pages

--- Website/frontend/src/App.jsx
+++ Website/frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import API from "./pages/API.jsx";
+import Footer from "./components/Footer.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
 
   return (
-    <>
-
-    </>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/api_page" element={<API />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
   )
 }
 
---
+++ Website/frontend/src/pages/API.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Api = () => {
+    return (
+        <div>
+            API_PAGE
+        </div>
+    );
+};
+
+export default Api;
