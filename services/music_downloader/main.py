57a57861c09134903f4e6c4d42c9f18d697708b2
Pavlo <pas.haors79@gmail.com>
2025-03-27 23:00:24 +0100

    add Dockerfile and docker-compose for music_downloader service

--- services/music_downloader/main.py
+++ services/music_downloader/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, send_from_directory
-from script.script import download_audio
 from flask_cors import CORS
 import os
+
+from script.script import download_audio
+
 app = Flask(__name__)
 CORS(app)
 
