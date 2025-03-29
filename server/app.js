006a00fda421569475c2f425e3bcc77864e7f1c5
Pavlo <pas.haors79@gmail.com>
2025-03-29 01:16:55 +0100

    create backend server with auth for user

---
+++ server/app.js
@@ -0,0 +1,32 @@
+import express, {json} from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url';
+import usersRouter from './routes/users-routes.js';
+import authRoutes from "./routes/auth-routes.js";
+
+dotenv.config();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express()
+const PORT = process.env.RORT || 5000;
+const corsOptions = {credentials: true, origin: process.env.URL || '*'};
+
+app.use(cors(corsOptions));
+app.use(json());
+app.use(cookieParser());
+
+// main
+
+app.use('/', express.static(join(__dirname, 'public')));
+app.use('/api/users', usersRouter);
+app.use('/api/auth', authRoutes);
+
+//
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ server/routes/auth-routes.js
@@ -0,0 +1,69 @@
+import express from "express";
+import pool from '../database/db.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import {jwtTokens} from '../utils/jwt-helpers.js'
+import err from "jsonwebtoken/lib/JsonWebTokenError.js";
+
+const router = express.Router();
+
+router.post('/login', async (req, res) => {
+    try{
+        const {email, password} = req.body;
+
+        const users = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
+
+        if(users.rows.length === 0) {
+            return res.status(404).json({error: 'Email is incorrect'});
+        }
+
+        //password check
+
+        const validPassword = await bcrypt.compare(password, users.rows[0].user_password);
+
+        if(!validPassword) {
+            return res.status(401).json({error: 'Password is incorrect'});
+        }
+
+        //JWT
+
+        let tokens = jwtTokens(users.rows[0]);
+        res.cookie('refresh_token', tokens.refreshToken, {httpOnly: true});
+        res.json(tokens);
+
+    }catch(err){
+        res.status(401).json({error: err.message});
+    }
+});
+
+router.get('/refresh_token', (req, res) => {
+    try{
+        const refresh_token = req.cookies.refreshToken;
+        if(refresh_token === null){
+            return res.status(401).json({error: 'Refresh token is null'});
+        }
+        jwt.verify(refresh_token, process.env.REFRESH_TOKEN_SECRET, (error, user) => {
+            if(error){
+                return res.status(401).json({error: error.message});
+            }
+            let tokens  = jwtTokens(user);
+            res.cookie('refresh_token', tokens.refreshToken, {httpOnly: true});
+            res.json(tokens);
+        })
+        console.log(refresh_token);
+    }catch(err){
+        res.status(401).json({error: err.message});
+    }
+});
+
+router.delete('/refresh_token', (req, res) => {
+    try{
+        res.clearCookie('refresh_token');
+        return res.status(200).json({message: 'Refresh token has been deleted'});
+    } catch{
+        res.status(401).json({error: err.message});
+    }
+});
+
+
+export default router;
